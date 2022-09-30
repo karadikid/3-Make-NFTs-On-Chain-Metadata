03f7c9ddfc77dfc8a9d0392d6da66da0c565eede
Gouthum Karadi <karadi@virtualnex.us>
2022-09-30 12:37:01 -0400

    Contract complete, compiled and deployed to Polygon Mumbai Testnet.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.10",
   networks: {
     mumbai: {
       url: process.env.TESTNET_RPC,
-      accounts: [process.env.PRIVATE_KEY]}
+      accounts: [process.env.PRIVATE_KEY]
+    },
   },
-    etherscan: {
-      apiKey: process.env.POLYGONSCAN_API_KEY
-    }
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  }
 };
